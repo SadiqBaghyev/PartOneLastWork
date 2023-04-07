d8a740e2c610a04d3868e18f606e7264fbb04833
SadiqBaghyev <sadiqbaghyev@gmail.com>
2023-04-07 13:38:27 +0400

    Task added

---
+++ Qutardayqutarrrrr/Models/Students.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qutardayqutarrrrr.Models
+{
+    internal class Students
+    {
+       private int id;
+       private string name;
+       private string surname;
+       private int GroupNumber;
+
+
+        public void ToalInfo(int id, string name, string surname)
+        {
+            this.id = id;
+            this.name = name;
+            this.surname = surname;
+        }
+
+      
+        public static int StudentsNumbers { get; set; } = 234;
+
+        public Students()
+        {
+            GroupNumber = StudentsNumbers++;
+            //Console.WriteLine(StudentsNumbers);
+            //Console.WriteLine(GroupNumber);
+        }
+        public void PrintInfo()
+        {
+
+            Console.WriteLine("Group name: " + GroupNumber);
+            Console.WriteLine("\tid     : " + id);
+            Console.WriteLine("\tname  : " + name);
+            Console.WriteLine("\tsurname      : " + surname);
+
+
+        }
+
+
+
+    }
+}
---
+++ Qutardayqutarrrrr/Program.cs
@@ -0,0 +1,26 @@
+﻿using Qutardayqutarrrrr.Models;
+
+namespace Qutardayqutarrrrr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Students[] students= new Students[2];
+
+            students[0] = new Students();
+            students[1] = new Students();
+
+            Group[] groups= new Group[2];
+            students[0].ToalInfo(132, "Sadiq", "Bhagiyev");
+            students[0].PrintInfo();
+
+            students[1].ToalInfo(122, "Ferrux", "Aliyev");
+            students[1].PrintInfo();
+            ///Console.WriteLine(Group.GroupNumbers);
+            //Console.WriteLine(Students.StudentsNumbers);
+            //students[0].PrintInfo();
+        }
+    }
+}
